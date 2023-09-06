ec9ea823c3a19f3893fd465da6694b04cecac54d
khouloud-kartal <114388142+khouloud-kartal@users.noreply.github.com>
2023-09-06 15:35:15 +0200

    Job 03 is finished

--- jour-01/job-01/script.js
+++ jour-01/job-01/script.js
@@ -1,20 +1,47 @@
 
 function myUpperCase(string){
 
-    const lowerCase = {'a' : 'A', 'b' : 'B', 'c' : 'C', 'd' : 'D', 'e' : 'E', 'f' : 'F', 'g': 'G', 'h': 'H', 'i' : 'I', 'j': 'J', 'k': 'K', 'l' : 'L', 'm' : 'M', 'n' : 'N', 'o' : 'O', 'p' : 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w' :'W', 'x' : 'X', 'y' : 'Y', 'z' : 'Z'};
+    const lowerCase = {
+        'a' : 'A',
+        'b' : 'B',
+        'c' : 'C',
+        'd' : 'D',
+        'e' : 'E',
+        'f' : 'F',
+        'g' : 'G',
+        'h' : 'H',
+        'i' : 'I',
+        'j' : 'J', 
+        'k' : 'K',
+        'l' : 'L',
+        'm' : 'M',
+        'n' : 'N',
+        'o' : 'O',
+        'p' : 'P',
+        'q' : 'Q',
+        'r' : 'R',
+        's' : 'S',
+        't' : 'T',
+        'u' : 'U',
+        'v' : 'V',
+        'w' : 'W',
+        'x' : 'X',
+        'y' : 'Y',
+        'z' : 'Z'
+    };
 
     
     let result = '';
 
-       for (const car of string) {
-            if (lowerCase[car]) {
-                result += lowerCase[car];
+       for (const char of string) {
+            if (lowerCase[char]) {
+                result += lowerCase[char];
             }else{
-                result += car;      
+                result += char;      
             }
         }
     
-        console.log(result);
+        return result;
 
 }
 
